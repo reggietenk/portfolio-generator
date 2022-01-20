f6e0b912a386fef08df8550d62f068290b866f5a
reggietenk <reggietenkorang@gmail.com>
2022-01-20 16:35:20 -0500

    HTML Set up

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-const message = 'Hello Node!';
-
-if (true === true) {
-  const message = 'Hello ES6!';
-  let sum = 5;
-  sum += 10;
-  console.log(message);
-  console.log(sum);
-}
-
-console.log(message);
-console.log(sum);
-
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
   
-}
-
-
-// printProfileData();
-
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${'reggietenk'}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+  console.log(name, github)
+  console.log(generatePage(name,github))
