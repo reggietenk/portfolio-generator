1ca39ccd946b5d956e25738b79a9550abe0ea9a8
reggietenk <reggietenkorang@gmail.com>
2022-01-20 21:55:07 -0500

    app.js correction

--- app.js
+++ app.js
@@ -117,9 +117,10 @@ promptUser()
 .then(portfolioData => {
 console.log(portfolioData);
 });
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-// if (err) throw err;
-
-// console.log('Portfolio complete! Check out index.html to see the output!');
+// will be uncommented in lesson 4
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
 // });
